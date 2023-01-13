73f53e1d3f2713582ddfefd4679ed2655410ebb3
[surena] <[mohammadkarimi.swctw@gmail.com]>
2023-01-13 18:57:44 +0330

    push all project

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <Fragment>
             <div className="border-b p-2 text-center">
-                <h1 className="text-2xl" style={{ color: "#0089ff" }}>Today Picture</h1>
+                <h1 className="text-2xl" style={{ color: "#0089ff" }}>Today's NASA image</h1>
             </div>
         </Fragment>
     )
